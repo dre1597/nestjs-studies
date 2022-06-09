b3648d3b0e5cc122cb838715d0e918d8c1132e2e
André Pereira <andrepereira97@yahoo.com>
2022-06-08 23:22:53 -0300

    :sparkles: clear student module and add graphql configs into it

---
+++ nestjs-graphql-nestjsquery/src/modules/students/dto/index.ts
@@ -0,0 +1,3 @@
+export * from './create-student.input';
+export * from './student.dto';
+export * from './update-student.input';
--- nestjs-graphql-nestjsquery/src/modules/students/students.module.ts
+++ nestjs-graphql-nestjsquery/src/modules/students/students.module.ts
@@ -1,8 +1,28 @@
+import {
+  NestjsQueryGraphQLModule,
+  PagingStrategies,
+} from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
 import { Module } from '@nestjs/common';
-import { StudentsService } from './students.service';
-import { StudentsResolver } from './students.resolver';
+import { CreateStudentInput, StudentDto, UpdateStudentInput } from './dto';
+import { Student } from './entities';
 
 @Module({
-  providers: [StudentsResolver, StudentsService]
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQueryTypeOrmModule.forFeature([Student])],
+      resolvers: [
+        {
+          DTOClass: StudentDto,
+          EntityClass: Student,
+          CreateDTOClass: CreateStudentInput,
+          UpdateDTOClass: UpdateStudentInput,
+          enableTotalCount: true,
+          pagingStrategy: PagingStrategies.OFFSET,
+        },
+      ],
+    }),
+  ],
+  providers: [],
 })
 export class StudentsModule {}
