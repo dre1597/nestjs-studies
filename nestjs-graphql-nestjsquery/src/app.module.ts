549fb83bb3bb9838f3cda8477e4fa35727212dfc
André Pereira <andrepereira97@yahoo.com>
2022-06-08 21:47:04 -0300

    :sparkles: add graphql configs

--- nestjs-graphql-nestjsquery/src/app.module.ts
+++ nestjs-graphql-nestjsquery/src/app.module.ts
@@ -1,8 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
   controllers: [],
   providers: [AppService],
 })
