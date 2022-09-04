0ccd9ebddcb046db4d9b9ee9b0614690d80e9c2e
André Pereira <andrepereira97@yahoo.com>
2022-09-03 22:59:08 -0300

    :sparkles: config mongoose and add list all products endpoint

--- scalable-scripts/main/src/app.module.ts
+++ scalable-scripts/main/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './modules/product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27018/nest_main', {
+      autoCreate: true,
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
