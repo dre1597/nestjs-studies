c84d8fb2f695d796d54ade44e7a7f4e4bb2f8361
André Pereira <andrepereira97@yahoo.com>
2023-02-13 23:10:48 -0300

    :recycle: move cache to the service

--- online-shop-backend/src/modules/city/city.controller.ts
+++ online-shop-backend/src/modules/city/city.controller.ts
@@ -1,23 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
 
-import { CustomCacheService } from '../../shared/custom-cache/custom-cache.service';
 import { FindAllCitiesByStateRo } from './boundary';
 import { CityService } from './city.service';
 
 @Controller('cities')
 export class CityController {
-  constructor(
-    private readonly cityService: CityService,
-    private readonly cacheService: CustomCacheService,
-  ) {}
+  constructor(private readonly cityService: CityService) {}
 
   @Get('/:stateId')
   async findAllByState(
     @Param('stateId') stateId: string,
   ): Promise<FindAllCitiesByStateRo[]> {
-    return this.cacheService.getCache<FindAllCitiesByStateRo[]>(
-      `state_${stateId}`,
-      () => this.cityService.findAllByState(stateId),
-    );
+    return this.cityService.findAllByState(stateId);
   }
 }
--- online-shop-backend/src/modules/city/city.service.ts
+++ online-shop-backend/src/modules/city/city.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+import { CustomCacheService } from '../../shared/custom-cache/custom-cache.service';
 import { FindAllCitiesByStateRo } from './boundary';
 import { CityRepository } from './repository';
 
 @Injectable()
 export class CityService {
-  constructor(private readonly cityRepository: CityRepository) {}
+  constructor(
+    private readonly cityRepository: CityRepository,
+    private readonly cacheService: CustomCacheService,
+  ) {}
 
   findAllByState(stateId: string): Promise<FindAllCitiesByStateRo[]> {
-    return this.cityRepository.findAllByState(stateId);
+    return this.cacheService.getCache<FindAllCitiesByStateRo[]>(
+      `state_${stateId}`,
+      () => this.cityRepository.findAllByState(stateId),
+    );
   }
 }
