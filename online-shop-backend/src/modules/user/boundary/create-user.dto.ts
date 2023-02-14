ac87c54b7e876f414d3db7ca853be044e1cfab94
André Pereira <andrepereira97@yahoo.com>
2023-02-13 23:31:27 -0300

    :sparkles: validate create user dto

--- online-shop-backend/src/modules/user/boundary/create-user.dto.ts
+++ online-shop-backend/src/modules/user/boundary/create-user.dto.ts
@@ -1,7 +1,24 @@
-export type CreateUserDto = {
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsEmail, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   name: string;
+
+  @IsString()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   password: string;
+
+  @IsEmail()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   email: string;
+
+  @IsString()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   phone: string;
+
+  @IsString()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   cpf: string;
-};
+}
