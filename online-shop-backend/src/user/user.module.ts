bb74eb03c77c8ba1b6e180ce5570661bab53edfd
André Pereira <andrepereira97@yahoo.com>
2023-02-25 17:05:33 -0300

    :ambulance: fix user module

--- online-shop-backend/src/user/user.module.ts
+++ online-shop-backend/src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { PrismaModule } from '../database/prisma.module';
 import { UserController } from './user.controller';
+import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService, PrismaModule],
+  providers: [UserService, UserRepository],
 })
 export class UserModule {}
