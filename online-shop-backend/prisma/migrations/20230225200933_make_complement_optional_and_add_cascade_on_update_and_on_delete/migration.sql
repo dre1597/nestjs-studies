bff1141057bbcad032ebc00c7290a7465a996715
André Pereira <andrepereira97@yahoo.com>
2023-02-25 17:10:36 -0300

    :sparkles: add nullable to complement, add on update and on delete actions

---
+++ online-shop-backend/prisma/migrations/20230225200933_make_complement_optional_and_add_cascade_on_update_and_on_delete/migration.sql
@@ -0,0 +1,31 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_City" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "stateId" TEXT NOT NULL,
+    CONSTRAINT "City_stateId_fkey" FOREIGN KEY ("stateId") REFERENCES "State" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_City" ("createdAt", "id", "name", "stateId", "updatedAt") SELECT "createdAt", "id", "name", "stateId", "updatedAt" FROM "City";
+DROP TABLE "City";
+ALTER TABLE "new_City" RENAME TO "City";
+CREATE TABLE "new_Address" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "complement" TEXT,
+    "numberAdress" INTEGER NOT NULL,
+    "cep" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "cityId" TEXT NOT NULL,
+    CONSTRAINT "Address_cityId_fkey" FOREIGN KEY ("cityId") REFERENCES "City" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Address" ("cep", "cityId", "complement", "createdAt", "id", "numberAdress", "updatedAt", "userId") SELECT "cep", "cityId", "complement", "createdAt", "id", "numberAdress", "updatedAt", "userId" FROM "Address";
+DROP TABLE "Address";
+ALTER TABLE "new_Address" RENAME TO "Address";
+CREATE UNIQUE INDEX "Address_userId_key" ON "Address"("userId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
