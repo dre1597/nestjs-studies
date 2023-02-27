7a8b11b7292a95815ff18c13c50745a1564989ab
André Pereira <andrepereira97@yahoo.com>
2023-02-26 22:23:17 -0300

    :construction: start city module and custom cache

--- online-shop-backend/src/app.module.ts
+++ online-shop-backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CustomCacheModule } from './custom-cache/custom-cache.module';
+import { CityModule } from './city/city.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CustomCacheModule, CityModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ online-shop-backend/src/city/city.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common';
+
+import { CityService } from './city.service';
+
+@Controller('city')
+export class CityController {
+  constructor(private readonly cityService: CityService) {}
+}
