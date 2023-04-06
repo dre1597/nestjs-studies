e035c4d5f365589f9badacfbb30db2081b61cb3c
André Pereira <andrepereira97@yahoo.com>
2023-04-05 22:42:08 -0300

    :sparkles: add swagger

--- nestjs-api-fc/src/main.ts
+++ nestjs-api-fc/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
       errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs demo api')
+    .setDescription('Demo api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
